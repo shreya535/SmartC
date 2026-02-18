238e7ce1997160f7f1609ffb5889d0eabc3e1eb0
shreya535 <165040844+shreya535@users.noreply.github.com>
2026-02-18 11:07:24 +0530

    Refactor input.c with constants and main function
    Refactor input.c to define constants and adjust variable scope.

--- input.c
+++ input.c
@@ -1,10 +1,12 @@
-int x = 10;
-float y;
+GNU nano 7.2                                                                                         input.c
+#define PI 3.14
+#define MAX 100
+#define A 10
 
-if (x > 5) {
-    y = x + 2;
+int main() {
+    float x = PI;
+    int y = MAX;
+    int z = A + 5;
+    return 0;
 }
-
-@invalid
-
 
