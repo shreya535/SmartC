70d0554ff70edfcfde8b8d61cd17070cbb264b78
shreya535 <165040844+shreya535@users.noreply.github.com>
2026-02-18 11:10:24 +0530

    Add files via upload

---
+++ compiler/input.c
@@ -0,0 +1,12 @@
+GNU nano 7.2                                                                                         input.c
+#define PI 3.14
+#define MAX 100
+#define A 10
+
+int main() {
+    float x = PI;
+    int y = MAX;
+    int z = A + 5;
+    return 0;
+}
+
